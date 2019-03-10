469f71007f51143ce2ad8d0afcacb3759962e03e
jsnhu <47902466+jsnhu@users.noreply.github.com>
2019-03-10 12:48:29 -0700

    added clue, fixed output

--- playtest.jl
+++ playtest.jl
@@ -1,5 +1,16 @@
 using JuMP, GLPKMathProgInterface
 
+# initial clue board (assume valid)
+clues = [   0 2 0 0 3 0 0 4 0;
+            6 0 0 0 0 0 0 0 3;
+            0 0 4 0 0 0 5 0 0;
+            0 0 0 8 0 6 0 0 0;
+            8 0 0 0 1 0 0 0 6;
+            0 0 0 7 0 5 0 0 0;
+            0 0 7 0 0 0 6 0 0;
+            4 0 0 0 0 0 0 0 8;
+            0 3 0 0 4 0 0 2 0]
+
 #=
     Sudoku board split into 9x9x9 cubic matrix.
     row i, column j, depth k
@@ -16,7 +27,16 @@ m = Model(solver = GLPKSolverMIP())
 # arbitrary objective function
 @objective(m, Max, sum(board[i,j,k] for i in 1:9, j in 1:9, k in 1:9))
 
-# constraints
+# clue constraints
+for i in 1:9
+    for j in 1:9
+        if clues[i,j] != 0
+            @constraint(m, board[i, j, clues[i,j]] == 1)
+        end
+    end
+end
+
+# rule constraints
 
 # cons1: each digit appears exactly once in each row
 for i in 1:9
@@ -57,7 +77,7 @@ sol_2d = Array{Int8}(undef, 9, 9)
 for k in 1:9
     for i in 1:9
         for j in 1:9
-            if solution[i,j,k] == 1
+            if sol_3d[i,j,k] == 1
                 sol_2d[i,j] = k
             end
         end
